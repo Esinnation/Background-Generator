75caeba31a2ac3be47f4daf4af33a514d00ddc80
Esinnation <abdulrazaqridollah96@gmail.com>
2022-07-04 02:11:23 -0700

    add a button which generates random gradient background

--- script.js
+++ script.js
@@ -2,7 +2,18 @@ const css = document.querySelector("h3");
 const color1 = document.querySelector(".color1");
 const color2 = document.querySelector(".color2");
 const body = document.getElementById("gradient");
+const randomBtn = document.getElementById("random-btn");
 
+css.textContent =` linear-gradient(to right, #00ff00 , #ff0000) ;`
+
+const generateRandomCOlor=()=>{
+	let letters="0123456789ABCDEF"
+	let color='#'
+	for(let i=0;i<6;i++){
+		color+=letters[Math.floor(Math.random()*16)]
+	}
+	return color
+}
 css.textContent =` linear-gradient(to right, #00ff00 , #ff0000) ;`
 
 function setGradient() {
@@ -15,6 +26,17 @@ function setGradient() {
 
 	css.textContent = body.style.background + ";";
 }
+
+randomBtn.addEventListener('click',function(){
+	body.style.background = 
+	`linear-gradient(to right,  
+	${generateRandomCOlor()} 
+	, 
+	${generateRandomCOlor()} 
+	)`;
+	css.textContent = body.style.background + ";";
+})
+
 
 color1.addEventListener("input", setGradient);
 
